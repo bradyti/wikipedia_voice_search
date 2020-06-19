e16fece5aee578bb6fcce73760fc4e40554d5c47
bradyti <33490200+bradyti@users.noreply.github.com>
2020-06-19 20:22:48 +0100

    Create search_wikipedia.py

---
+++ search_wikipedia.py
@@ -0,0 +1,28 @@
+import speech_recognition as sr
+import pyttsx3
+import wikipedia
+
+def speak(string):
+    engine = pyttsx3.init()
+    engine.say(string)
+    engine.runAndWait()
+
+def userInput():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Speak Now....")
+        audio = r.listen(source)
+        query = r.recognize_google(audio)
+    return query
+
+def out():
+    query = userInput().lower()
+    print(f"User said: {query}")
+    results = wikipedia.summary(query, sentences=3)
+    print(results)
+    speak("Wikipedia Said: ")
+    speak(results)
+
+speak("Tell me your query")
+speak("I will search wikipedia for you")
+out()
